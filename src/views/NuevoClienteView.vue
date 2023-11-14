b85fb2c0baec82b400094e7a3df795e7c702fcf6
Javivzk <javi.vzk@gmail.com>
2023-11-14 14:36:11 +0100

    Optimizando codigo

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,6 +1,13 @@
 
 <script setup>
     import RouterLink from '../components/UI/RouterLink.vue';
+    import Heading from '../components/UI/Heading.vue';
+
+    defineProps({
+        titulo: {
+            type: String
+        }
+    })
 </script>
 
 <template>
@@ -11,6 +18,6 @@
             </RouterLink>
         </div>
 
-        <h1 class="text-4xl font-extrabold text-slate-500">Agregar Cliente</h1>
+        <Heading>{{ titulo }}</Heading>
     </div>
 </template>
