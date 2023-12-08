f5c7df3fb4a5afd91de3aafe6f8c13a77ce000cd
Javivzk <javi.vzk@gmail.com>
2023-12-08 17:58:50 +0100

    Funcionalidades completas

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,15 +1,27 @@
+// axios.js
 import axios from 'axios';
+import { useAuthStore } from '@/stores/auth';
+import { onMounted } from 'vue';
 
 axios.defaults.withCredentials = true;  // Habilita el envío de cookies
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api'
+  baseURL: 'http://localhost:8080/api',
 });
 
-axios.interceptors.request.use(function (config) {
-    const token = localStorage.getItem('userToken');
-    config.headers.Authorization = token ? `Bearer ${token}` : '';
-    return config;
+// Intercepta todas las solicitudes y agrega el token al encabezado de autorización si está presente
+api.interceptors.request.use((config) => {
+  const authStore = useAuthStore();  // Mueve esta línea al interceptor
+  const token = authStore.token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// Asegúrate de ejecutar esta configuración después de que Pinia esté activa
+onMounted(() => {
+  // Puedes realizar alguna acción aquí si es necesario
 });
 
 export default api;
